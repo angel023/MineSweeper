23011b7314b6d7cf49be2a49cbe8691982631474
Kalojanatanasov <Kalojanatanasov@gmail.com>
2013-04-24 00:35:20 +0300

    continue minor refactoring add TO DO comments for replace of GO TO command

--- Minesweeper-2/Telerik.cs
+++ Minesweeper-2/Telerik.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;gameField
+using System.Linq;gameBoard
 using System.Text;
 
 // Initial commit!
@@ -8,15 +8,15 @@ using System.Text;
 namespace Minesweeper
 {
     class Telerik
-    {
+    {    
         private static int[,] arrayOfMines=new int[5,10];
         private static int[] randomNumber = new int[15];
-        private static int[,] gameField= new int[5,10];
+        private static int[,] gameBoard= new int[5,10];
         private static int[,] open = new int[5, 10]; 
         private static int[] playerScore = new int[5];
         private static string[] playerName = new string[5];
         private static int playerScoreCounter = 0;
-
+        
         private static bool IsFoundInRandomNumbers(int index, int number) 
         {
             bool result = false;
@@ -35,14 +35,14 @@ namespace Minesweeper
 
         }
 
-        private static void InitializearrayOfMines()
+        private static void InitializeArrayOfMines()
         {
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
                     arrayOfMines[i, j] = 0;
-                    gameField[i, j] = 0;
+                    gameBoard[i, j] = 0;
                     open[i, j] = 0;
                 }
             }
@@ -52,49 +52,43 @@ namespace Minesweeper
             for (int i = 0; i < 15; i++)
             {
                 int index = random.Next(50);
-               
 
                 while (IsFoundInRandomNumbers(i, index))
                 {
                     index = random.Next(50);
                 }
-
-               // Console.WriteLine("{0},{1},{2}",index,(index/10),(index % 10));
-                randomNumber[i] = index;
-               
-
-
-                arrayOfMines[(index / 10), (index % 10)] = 1;
                 
+                randomNumber[i] = index;
+                arrayOfMines[(index / 10), (index % 10)] = 1;                
             }
         }
 
-        private static void DisplayarrayOfMines()
+        private static void DisplayArrayOfMines()
         {
             Console.Write("    ");
+
             for (int i = 0; i < 10; i++)
             {
                 Console.Write("{0} ", i);
             }
-            Console.WriteLine("");
 
+            Console.WriteLine("");
             Console.Write("    ");
+
             for (int i = 0; i < 21; i++)
             {
                 Console.Write("-");
             }
+
             Console.WriteLine("");
 
             for (int i = 0; i < 5; i++)
             {
                 for(int j=0;j<13;j++)
-
-
-
                 {
                     if(2<=j && j<=11)
                     {
-                        if(gameField[i,j-2]==0)
+                        if(gameBoard[i,j-2]==0)
                         {
                             Console.Write("? ");
                         }
@@ -106,8 +100,14 @@ namespace Minesweeper
                             }
                             else
                             {
-                                if (open[i, j - 2] == 1) Console.Write("{0} ", CountNeighborcell(i, j - 2));
-                                else Console.Write("- ");
+                                if (open[i, j - 2] == 1)
+                                {
+                                    Console.Write("{0} ", CountNeighborcell(i, j - 2));
+                                }
+                                else 
+                                {
+                                    Console.Write("- ");
+                                }
                             }
                         }
                     }
@@ -119,20 +119,18 @@ namespace Minesweeper
                     {
                         Console.Write("{0} ", i);
                     }
-
                 }
                 Console.WriteLine("");
             }
 
             Console.Write("    ");
+
             for (int i = 0; i < 21; i++)
             {
                 Console.Write("-");
             }
 
-
             Console.WriteLine("");
-
         }
 
         private static int CountNeighborcell(int i,int j)
@@ -146,7 +144,7 @@ namespace Minesweeper
                 {
                     if (j1 == 0 && i1 == 0)
                         continue;
-                    if (proverka(i + i1, j + j1) && arrayOfMines[i + i1, j + j1] == 1)
+                    if (CheckBoardDimensions(i + i1, j + j1) && arrayOfMines[i + i1, j + j1] == 1)
                     {
                         counter++;
                     }
@@ -156,52 +154,37 @@ namespace Minesweeper
             return counter;
         }
 
-        /*\
-         * 
-         * 
-         * 
-         * 
-         **********************
-         **
-         *****
-         *
-         * 
-         *  tui e nqkvo krasivo
-         * krasotata e hubavo neshto...
-         * 
-         * 
-         * 
-         */
-
         private static void DisplayTop()
         {
-            Console.WriteLine("Scoreboard:\n");
-            for (int i = 0; i < (playerScoreCounter)%6; i++)
+            Console.WriteLine("Scoreboard: {0}", Environment.NewLine);
+            for (int i = 0; i < playerScoreCounter%6; i++)
             {
                 Console.WriteLine("{0}. {1} --> {2} cells",i,playerName[i],playerScore[i]);
             }
         }
 
-        private static bool proverka(int i,int j)
+        private static bool CheckBoardDimensions(int i,int j)
         {
             return ( 0<=i && i<=4) && (0<=j && j<=9);
         }
 
         private static int CountOpen()
         {
-            int res = 0;
+            int result = 0;
             for(int i=0;i<5;i++)
                 for (int j = 0; j < 10; j++)
                 {
                     if (open[i, j] == 1)
-                        res++;
+                    {
+                        result++;
+                    }
                 }
-            return res;
+            return result;
         }
 
         static void Main(string[] argumenti)
         {
-            //InitializearrayOfMines();
+            //InitializeArrayOfMines();
            /* for (int i = 0; i < 15; i++)
             {
                 Console.WriteLine(1);
@@ -217,72 +200,99 @@ namespace Minesweeper
             //}
 
             //Console.WriteLine("{0}", CountNeighborcell(3,0));
-            //gameField[1, 3] = 1;
-            //gameField[2, 4] = 1;
-            //gameField[0, 0] = 1;
-            //gameField[2, 1] = 1;
-            //gameField[1, 0] = 1;
+            //gameBoard[1, 3] = 1;
+            //gameBoard[2, 4] = 1;
+            //gameBoard[0, 0] = 1;
+            //gameBoard[2, 1] = 1;
+            //gameBoard[1, 0] = 1;
 
-            //DisplayarrayOfMines();
+            //DisplayArrayOfMines();
+
+            //TO DO: Some REFACTORING Used for go to !!!
             begin:
-            // tuka skachame kogaot iskame da begin-nem
-
-            Console.WriteLine("Welcome to the game “Minesweeper”.\nTry to reveal all cells without mines. Use 'top' to view the scoreboard,\n'restart' to start a new game and 'exit' to quit the game.");
+           
+            Console.WriteLine("Welcome to the game “Minesweeper”.{0} Try to reveal all cells without mines. Use 'TOP' to view the scoreboard, {0}'RESTART' to start a new game and 'EXIT' to quit the game.", Environment.NewLine);
         
-            InitializearrayOfMines();
-  //tui "f:" e adski qko a?
-            f:
-            Console.WriteLine("\nEnter row and column: ");
-            string p = (Console.ReadLine());
+            InitializeArrayOfMines();
 
-            if (p.Equals("restart"))
-            { goto begin; }
+            //TO DO:Refactoring with While!!!
+             f:
+            Console.WriteLine("{0} Please input your move: ",Environment.NewLine);
+            string playersMove = Console.ReadLine();
 
-            if (p.Equals("top"))
-            { DisplayTop(); goto begin; }
+            //TO DO: Must be refactored!!!
+            if (playersMove.Equals("restart"))
+            { 
+                goto begin; 
+            }
 
-            if (p.Equals("exit"))
+            if (playersMove.Equals("top"))
+            { 
+                DisplayTop(); 
+                goto begin; 
+            }
+
+            if (playersMove.Equals("exit"))
+            {
                 goto end;
+            }
 
             // MAIN
-            if (p.Length < 3)
-            { Console.WriteLine("Illegal input"); goto f; }
-            int p1 = Convert.ToInt32((p.ElementAt(0)).ToString());
-
-            int p2 = Convert.ToInt32((p.ElementAt(2)).ToString());
-            Console.WriteLine(p1);
-           
-            if (open[p1, p2] == 1)
-            { Console.WriteLine("Illegal move!"); goto f; }
-
-            if (open[p1, p2] == 0)
-            {
-                open[p1, p2] = 1;
-                gameField[p1, p2] = 1;
-                if (arrayOfMines[p1, p2] == 1)
-                {
-                    for (int i = 0; i < 5; i++)
-                        for (int j = 0; j < 10; j++)
-                        { gameField[i, j] = 1; }
-                    DisplayarrayOfMines();
-                    Console.WriteLine("Booooom! You were killed by a mine. You revealed 2 cells without mines.Please enter your name for the top scoreboard:");
-                    string str = Console.ReadLine();
-                    playerName[playerScoreCounter % 5] = str;
-                    playerScore[playerScoreCounter % 5] = CountOpen() - 1;
-                    goto begin;
-                }
-                Console.WriteLine(CountNeighborcell(p1, p2));
-                DisplayarrayOfMines();
+            if (playersMove.Length < 3)
+            { 
+                Console.WriteLine("Illegal input");
                 goto f;
             }
 
+            int moveToRow = Convert.ToInt32((playersMove.ElementAt(0)).ToString());
 
+            int moveToColumn = Convert.ToInt32((playersMove.ElementAt(2)).ToString());
 
-            //Console.WriteLine(w==q);
+            Console.WriteLine(moveToRow);
+           
+            if (open[moveToRow, moveToColumn] == 1)
+            { 
+                Console.WriteLine("Illegal move!"); 
+                goto f;
+            }
+
+            if (open[moveToRow, moveToColumn] == 0)
+            {
+                open[moveToRow, moveToColumn] = 1;
+                gameBoard[moveToRow, moveToColumn] = 1;
+
+                if (arrayOfMines[moveToRow, moveToColumn] == 1)
+                {
+                    for (int i = 0; i < 5; i++)
+                        for (int j = 0; j < 10; j++)
+                        { 
+                            gameBoard[i, j] = 1;
+                        }
+
+                        DisplayArrayOfMines();
+                        Console.WriteLine("Booooom! You were killed by a mine. {0} You score is {1}. Please enter your name for the top scoreboard: ", Environment.NewLine, playerScoreCounter);
+
+                    string name = Console.ReadLine();
+
+                    playerName[playerScoreCounter % 5] = name;
+                    playerScore[playerScoreCounter % 5] = CountOpen() - 1;
+
+                    goto begin;
+
+                }
+
+                Console.WriteLine(CountNeighborcell(moveToRow, moveToColumn));
+
+                DisplayArrayOfMines();
+
+                goto f;
+
+            }
             Console.WriteLine();
 
-        end:
-            Console.WriteLine("Good Bye");
+            end:
+
+            Console.WriteLine("Good Bye!!!");
         }
     }
 }
