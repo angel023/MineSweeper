c1148ef5bd309d023b2e3f477d7dc5dbd98a277b
Boris Matev <lawoffive@gmail.com>
2013-05-14 21:59:45 +0300

    added struct Player
    It has three properties - Name, Score и Move. Score is initialized with
    0 by the constructor. Move can be used instead of playerMove. Name and
    Score are used only when the game ends for the ranking. This way,
    instead of arrays playerScore and playerName, one can be used with
    Player-s.

---
+++ Minesweeper/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinesweeperProject
+{
+    struct Player
+    {
+        public int Score { get; set; }
+        public int Name { get; set; }
+        public string Move { get; set; }
+        
+        public Player() 
+        {
+            this.Score = 0;
+        }
+    }
+}
