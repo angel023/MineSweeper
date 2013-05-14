57a45559c6b739e58f2ba3bbfabf60a79f5463ac
Tankovski <stoian.tankovski@gmail.com>
2013-05-14 22:18:42 +0300

    Refacturing of the Main method
    I extract a lot of things form the Main method in new methods. And I
    deleted all comments.

--- Minesweeper/MinesweeperMain.cs
+++ Minesweeper/MinesweeperMain.cs
@@ -1,37 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-// Removed 'gameBoard' after System.Linq
 using System.Linq;
 using System.Text;
 
-// Initial commit!
-
 namespace MinesweeperProject
 {
-    //rename class form ttelrik to current
     class MinesweeperMain
     {
-        // rename everywhere  atrica to arrayofMines
         private static int[,] arrayOfMines = new int[5, 10];
-        // rename everywhere  nekviChisla to randomNumber.......I maked numbers becouse is not only one number
         private static int[] randomNumbers = new int[15];
-        //rename everywhere state to gameBoard......I think gameField is little better
         private static int[,] gameField = new int[5, 10];
-
-        //rename <open> to <isCellOpen>
-        //I rename it to OpenCells becouse this array hold the information for the open cells, and isCellOpen is more 
-        //appropriate for boolean variable
         private static int[,] OpenCells = new int[5, 10];
-
-        // rename topCells to playerScore
-        private static int[] playerScore = new int[5];
-
-        // rename everywhere topNames to playerName
-        private static string[] playerName = new string[5];
-        // rename topCellsCounter to playerScoreCounter
+        private static int[] playersScores = new int[5];
+        private static string[] playersNames = new string[5];
         private static int playerScoreCounter = 0;
-
-        //delete some empty lines
         private static bool IsFoundInRandomNumbers(int index, int number)
         {
             bool result = false;
@@ -46,7 +28,6 @@ namespace MinesweeperProject
             return result;
         }
 
-        //This method initialise all game field not only the mines array
         private static void InitializeNewGameField()
         {
             ClearTheField();
@@ -54,12 +35,11 @@ namespace MinesweeperProject
             InitialiseTheMinesOnTheField();
         }
 
-        //I extracted this like a new method
         private static void ClearTheField()
         {
-            for (int row = 0; row < gameField.GetLength(0); row++) //change i to row and make magic number 5 to gameField.GetLength(0);
+            for (int row = 0; row < gameField.GetLength(0); row++) 
             {
-                for (int col = 0; col < gameField.GetLength(1); col++)// change j to col and make magic number 10 to gameField.GetLength(1);
+                for (int col = 0; col < gameField.GetLength(1); col++)
                 {
                     arrayOfMines[row, col] = 0;
                     gameField[row, col] = 0;
@@ -68,7 +48,6 @@ namespace MinesweeperProject
             }
         }
 
-        //I extracted this like a new method and renamed everything inside
         private static void InitialiseTheMinesOnTheField()
         {
             Random randomGenerator = new Random();
@@ -87,8 +66,6 @@ namespace MinesweeperProject
             }
         }
 
-        //I changed the name of DisplayArrayOfMines with DisplayGameField because this method load the whole field.
-        //And make some refactoring
         private static void DisplayGameField()
         {
             DisplayColNumbers();
@@ -156,76 +133,6 @@ namespace MinesweeperProject
             Console.WriteLine();
         }
 
-        //private static void DisplayGameField()
-        //{
-        //    Console.Write("    ");
-
-        //    for (int i = 0; i < 10; i++)
-        //    {
-        //        Console.Write("{0} ", i);
-        //    }
-
-        //    Console.WriteLine("");
-        //    Console.Write("    ");
-
-        //    for (int i = 0; i < 21; i++)
-        //    {
-        //        Console.Write("-");
-        //    }
-
-        //    Console.WriteLine("");
-
-        //    for (int i = 0; i < 5; i++)
-        //    {
-        //        for (int j = 0; j < 13; j++)
-        //        {
-        //            if (2 <= j && j <= 11)
-        //            {
-        //                if (gameBoard[i, j - 2] == 0)
-        //                {
-        //                    Console.Write("? ");
-        //                }
-        //                else
-        //                {
-        //                    if (arrayOfMines[i, j - 2] == 1)
-        //                    {
-        //                        Console.Write("* ");
-        //                    }
-        //                    else
-        //                    {
-        //                        if (isCellOpen[i, j - 2] == 1)
-        //                        {
-        //                            Console.Write("{0} ", CountSurroundingMines(i, j - 2));
-        //                        }
-        //                        else
-        //                        {
-        //                            Console.Write("- ");
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //            if (j == 1 || j == 12)
-        //            {
-        //                Console.Write("| ");
-        //            }
-        //            if (j == 0)
-        //            {
-        //                Console.Write("{0} ", i);
-        //            }
-        //        }
-        //        Console.WriteLine("");
-        //    }
-
-        //    Console.Write("    ");
-
-        //    for (int i = 0; i < 21; i++)
-        //    {
-        //        Console.Write("-");
-        //    }
-
-        //    Console.WriteLine("");
-        //}
-
         private static int CountSurroundingMines(int currentRow, int currentCol)
         {
             int minesCounter = 0;
@@ -249,26 +156,18 @@ namespace MinesweeperProject
             return minesCounter;
         }
 
-        //removed nonsence comments
-        //I fixed big bug here now displayTop works good
-        //I renamed to displayScoreBoard becouse this is what actually this method do
         private static void DisplayScoreBoard()
         {
             Console.WriteLine("Scoreboard: {0}", Environment.NewLine);
-            //for (int i = 0; i < playerScoreCounter % 6; i++)
-            for (int i = 0; i < playerScore.Length; i++)
+            for (int i = 0; i < playersScores.Length; i++)
             {
-                Console.WriteLine("{0}. {1} --> {2} cells", i, playerName[i], playerScore[i]);
+                Console.WriteLine("{0}. {1} --> {2} cells", i, playersNames[i], playersScores[i]);
             }
         }
-
-        //refactor the method in quallity terms origirn is return ( 0<=i && i<=4) && (0<=j && j<=9)
-        //I also make little canges here I hope you liked better this way? And also change the name becouse this method
-        //check is the position valid not the dimensions
-        //Changed CheckIsThePosiotionValid to IsPositionValid
+       
         private static bool IsPositionValid(int row, int col)
         {
-            bool isRowValid = false; //Its more appropriate the name of bool to start with "Is"
+            bool isRowValid = false; 
             if (0 <= row && row <= 4)
             {
                 isRowValid = true;
@@ -277,16 +176,14 @@ namespace MinesweeperProject
             bool isColumnValid = false;
             if (0 <= col && col <= 9)
             {
-                isColumnValid = true; //Its more appropriate the name of bool to start with "Is"
+                isColumnValid = true; 
             }
 
-            bool isValidPosition = (isRowValid && isColumnValid); //This is validation for the position not for the dimensions
+            bool isValidPosition = (isRowValid && isColumnValid); 
 
             return isValidPosition;
         }
 
-        // rename res to result 
-        //Rename the method to CountOpenCells an rename some variables
         private static int CountOpenCells()
         {
             int result = 0;
@@ -303,71 +200,72 @@ namespace MinesweeperProject
             return result;
         }
 
-        static void Main(string[] argumenti)
+        private static void MakeAMove(int moveToRow, int moveToColumn)
         {
-            //InitializeNewGameField();
-            /* for (int i = 0; i < 15; i++)
-             {
-                 Console.WriteLine(1);
-                 Console.WriteLine(randomNumbers[i]);
-             }*/
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    for (int j = 0; j < 10; j++)
-            //    {
-            //        Console.Write(arrayOfMines[i, j]);
-            //    }
-            //    Console.WriteLine();
-            //}
+            OpenCells[moveToRow, moveToColumn] = 1;
+            gameField[moveToRow, moveToColumn] = 1;
+            playerScoreCounter += 1;
+        }
 
-            //Console.WriteLine("{0}", CountSurroundingMines(3,0));
-            //gameBoard[1, 3] = 1;
-            //gameBoard[2, 4] = 1;
-            //gameBoard[0, 0] = 1;
-            //gameBoard[2, 1] = 1;
-            //gameBoard[1, 0] = 1;
+        private static void EnterPlayerResult()
+        {
+            string name = Console.ReadLine();
 
-            //DisplayGameField();
+            Array.Sort(playersScores);
+            for (int i = 0; i < playersScores.Length; i++)
+            {
+                if (playerScoreCounter > playersScores[i])
+                {
+                    playersScores[i] = playerScoreCounter - 1;
+                    playersNames[i] = name;
+                    break;
+                }
+            }
+            playerScoreCounter = 0;
+        }
 
-            //TO DO: Some REFACTORING Used for go to !!!
+        private static void RevealGameField()
+        {
+            for (int boardRow = 0; boardRow < gameField.GetLength(0); boardRow++) 
+            {                                                                     
+                for (int boardCol = 0; boardCol < gameField.GetLength(1); boardCol++) 
+                {                                                                       
+                    gameField[boardRow, boardCol] = 1;
+                }
+            }
+        }
 
-            // Changed playersMove to playerMove. The string is initialized before the loop starts. TO DO: maybe refactor to avoid repeating code.
-            string playerMove = "";
+        private static void StartNewGame()
+        {
             Console.WriteLine("Welcome to the game “Minesweeper”.{0} Try to reveal all cells without mines. Use 'TOP' to view the scoreboard, {0}'RESTART' to start a new game and 'EXIT' to quit the game.", Environment.NewLine);
-
-            //Initialise the field in the beginning of the game
             Console.WriteLine();
             InitializeNewGameField();
             DisplayGameField();
+        }
 
-            while (playerMove != "exit")//It must be exit not end
+        static void Main()
+        {
+
+            StartNewGame();
+
+            string playerMove = "";
+            while (playerMove != "exit")
             {
                 Console.WriteLine("{0} Please input your move: ", Environment.NewLine);
                 playerMove = Console.ReadLine();
-                // rename p1 to moveToRow
+
                 int moveToRow;
                 int moveToColumn;
 
-                // Instead of begin:
                 if (playerMove == "top" || playerMove == "restart")
                 {
                     if (playerMove == "top")
                     {
                         DisplayScoreBoard();
                     }
-                    Console.WriteLine("Welcome to the game “Minesweeper”.{0} Try to reveal all cells without mines."+
-                        " Use 'TOP' to view the scoreboard, {0}'RESTART' to start a new game and 'EXIT' to quit the game.",
-                        Environment.NewLine);
 
-                    //Initialise the field with the start of the game
-                    Console.WriteLine();
-                    InitializeNewGameField();
-                    DisplayGameField();
+                    StartNewGame();
                 }
-                //Instead of f:. I also added a check to see if the command is correct
-                //I add a check for the space between the row and col input, becouse I think it is not correct input 123 for example
-                //and this will be correct without this check, but I still dont like it.Maybe we must think for some method for 
-                //parsing the input.
                 else if (playerMove.Length == 3 && int.TryParse(playerMove[0].ToString(), out moveToRow)
                     && int.TryParse(playerMove[2].ToString(), out moveToColumn) && playerMove[1].ToString() == " ")
                 {                                                                                                   
@@ -375,42 +273,14 @@ namespace MinesweeperProject
 
                     if (OpenCells[moveToRow, moveToColumn] == 0)
                     {
-                        OpenCells[moveToRow, moveToColumn] = 1;
-                        gameField[moveToRow, moveToColumn] = 1;
-                        playerScoreCounter += 1;
+                        MakeAMove(moveToRow, moveToColumn);
+
                         if (arrayOfMines[moveToRow, moveToColumn] == 1)
-                        {
-                            for (int boardRow = 0; boardRow < gameField.GetLength(0); boardRow++) //I changed i with boardRow and the lenght of the
-                            {                                                                     //loop with gameField.GetLength(0) instead magic number 5   
-                                for (int boardCol = 0; boardCol < gameField.GetLength(1); boardCol++) //I changed j with boardCol and the lenght of the
-                                {                                                                     //loop with gameField.GetLength(1) instead magic number 10   
-                                    gameField[boardRow, boardCol] = 1;
-                                }
-                            }
-
+                        {  
+                            RevealGameField();
                             DisplayGameField();
-                            // rewrite some of the message 
                             Console.WriteLine("Booooom! You were killed by a mine. {0} You score is {1}. Please enter your name for the top scoreboard: ", Environment.NewLine, playerScoreCounter-1);
-
-                            string name = Console.ReadLine();
-
-                            //Very big bug fixed! This way we can make gameboard, becouse in the original way it was no sorted.
-                            Array.Sort(playerScore);
-                            for (int i = 0; i < playerScore.Length; i++)
-                            {
-                                if (playerScoreCounter>playerScore[i])
-                                {
-                                    playerScore[i] = playerScoreCounter-1;
-                                    playerName[i] = name;
-                                    break;
-                                }
-                            }
-                            playerScoreCounter = 0;
-                            //playerName[playerScoreCounter % 5] = name;
-                            //playerScore[playerScoreCounter % 5] = CountOpenCells() - 1;
-                            
-                            //This was a big bug!!!After game over starts a new game on the old field and the old field is already
-                            //open so every move is market like not valid
+                            EnterPlayerResult();
                             InitializeNewGameField();
                             DisplayGameField();
                         }
@@ -426,13 +296,11 @@ namespace MinesweeperProject
                         Console.WriteLine("Illegal move!");
                     }
                 }
-                // Change from "(playersMove.Length < 3)". Now it checks for all invalid commands.
                 else if (playerMove != "exit")
                 {
                     Console.WriteLine("Illegal input");
                 }
             }
-            // remove //Console.WriteLine(w==q);
             Console.WriteLine();
 
             Console.WriteLine("Good Bye!!!");
