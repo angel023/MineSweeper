2a8e8f7be610758853030e423b5ee24a5e7d6836
Tankovski <stoian.tankovski@gmail.com>
2013-05-17 00:14:24 +0300

    Add GameField unit tests
    Test almostall methods in GameField class

--- MinesweeperProjectTest/GameFieldTests.cs
+++ MinesweeperProjectTest/GameFieldTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MinesweeperProject;
+using System.Text;
 
 namespace MinesweeperProjectTest
 {
@@ -18,7 +19,7 @@ namespace MinesweeperProjectTest
         public void GameFieldConstructorWithNegativeNumbers()
         {
             GameField gameField = new GameField(-5, 10);
-            
+
         }
 
         [TestMethod]
@@ -73,8 +74,167 @@ namespace MinesweeperProjectTest
 
             gameField.OpenCells[0, 2] = 1;
             var result = gameField.IsCellOpen(0, 10);
-
+            
             Assert.AreEqual(result, true);
         }
+
+        [TestMethod]
+        public void ClearTheFieldTest()
+        {
+            GameField gameField = new GameField(5, 10);
+            gameField.Field[0, 1] = 1;
+
+            PrivateObject obj = new PrivateObject(gameField);
+            obj.Invoke("ClearTheField");
+
+            Assert.AreEqual(gameField.Field[0, 1], 0);
+        }
+
+        [TestMethod]
+        public void RevealGameFieldTest()
+        {
+            GameField gameField = new GameField(5, 10);
+            gameField.RevealGameField();
+
+            for (int row = 0; row < gameField.Field.GetLength(0); row++)
+            {
+                for (int col = 0; col < gameField.Field.GetLength(1); col++)
+                {
+                    Assert.AreEqual(gameField.Field[row, col], 1);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void IsPositionValidForValidPositionTest()
+        {
+            GameField gameField = new GameField(5, 10);
+
+            PrivateObject obj = new PrivateObject(gameField);
+            var valid = obj.Invoke("IsPositionValid", 1, 2);
+
+            Assert.AreEqual(valid, true);
+        }
+
+        [TestMethod]
+        public void IsPositionValidForInValidPositionTest()
+        {
+            GameField gameField = new GameField(5, 10);
+
+            PrivateObject obj = new PrivateObject(gameField);
+            var valid = obj.Invoke("IsPositionValid", 10, 2);
+
+            Assert.AreEqual(valid, false);
+        }
+
+
+        [TestMethod]
+        public void CountOpenCellsTest()
+        {
+            GameField gameField = new GameField(5, 10);
+            gameField.OpenCells[0, 1] = 1;
+            gameField.OpenCells[3, 3] = 1;
+            gameField.OpenCells[2, 1] = 1;
+
+            PrivateObject obj = new PrivateObject(gameField);
+            var numberOfOpenCells = obj.Invoke("CountOpenCells");
+
+            Assert.AreEqual(numberOfOpenCells, 3);
+        }
+
+        [TestMethod]
+        public void GetSingleFieldRowStringTest()
+        {
+            GameField gameField = new GameField(5, 10);
+
+            PrivateObject obj = new PrivateObject(gameField);
+            var row = obj.Invoke("GetSingleFieldRowString", 1);
+            string expected = "? ? ? ? ? ? ? ? ? ? ";
+
+            Assert.AreEqual(row, expected);
+        }
+
+        [TestMethod]
+        public void GetSingleFieldRowStringWithMinesAndVisitedCellTest()
+        {
+            GameField gameField = new GameField(5, 10);
+            gameField.ArrayOfMines[1, 2] = 1;
+            gameField.ArrayOfMines[1, 4] = 1;
+            gameField.OpenCells[1, 3] = 1;
+            gameField.Field[1, 3] = 1;
+            PrivateObject obj = new PrivateObject(gameField);
+            var row = obj.Invoke("GetSingleFieldRowString", 1);
+            string expected = "? ? ? 2 ? ? ? ? ? ? ";
+
+            Assert.AreEqual(row, expected);
+        }
+
+        [TestMethod]
+        public void GetColNumbersStringTest()
+        {
+            GameField gameField = new GameField(5, 10);
+            StringBuilder sb = new StringBuilder();
+            PrivateObject obj = new PrivateObject(gameField);
+            sb.Append("    0 1 2 3 4 5 6 7 8 9 ");
+            sb.AppendLine();
+            var result = obj.Invoke("GetColNumbersString");
+            string expected = sb.ToString();
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void GetHorizontalWallStringTest()
+        {
+            GameField gameField = new GameField(5, 10);
+            StringBuilder sb = new StringBuilder();
+            PrivateObject obj = new PrivateObject(gameField);
+            sb.Append("   ---------------------");
+            sb.AppendLine();
+            var result = obj.Invoke("GetHorizontalWallString");
+            string expected = sb.ToString();
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void IsFoundInRandomNumbersTest()
+        {
+            GameField gameField = new GameField(5, 10);
+            StringBuilder sb = new StringBuilder();
+            PrivateObject obj = new PrivateObject(gameField);
+            int[] array = new int[] { 1, 5, 3 };
+            var result = obj.Invoke("IsFoundInRandomNumbers", 3, 5, array);
+            bool expected = true;
+
+            Assert.AreEqual(result, expected);
+        }
+
+        //[TestMethod]
+        //public void ToStringTest()
+        //{
+        //    GameField gameField = new GameField(0, 10);
+        //    gameField.ArrayOfMines[0, 2] = 1;
+        //    gameField.ArrayOfMines[0, 4] = 1;
+        //    gameField.OpenCells[0, 3] = 1;
+        //    gameField.Field[0, 3] = 1;
+        //    StringBuilder sb = new StringBuilder();
+        //    sb.Append("    0 1 2 3 4 5 6 7 8 9 ");
+        //    sb.AppendLine();
+        //    sb.Append("   ---------------------");
+        //    sb.AppendLine();
+        //    sb.AppendLine("0 | ");
+        //    sb.Append("? ? ? 2 ? ? ? ? ? ? ");
+        //    sb.AppendLine("|");
+        //    sb.Append("   ---------------------");
+        //    sb.AppendLine();
+        //    sb.AppendLine();
+
+        //    var result = gameField.ToString();
+
+        //    string expected = sb.ToString();
+
+        //    Assert.AreEqual(result, expected);
+        //}
     }
 }
