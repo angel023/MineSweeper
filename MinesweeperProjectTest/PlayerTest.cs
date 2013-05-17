3579376cac0795ba8268b24486646d25a3ae0662
Boris Matev <lawoffive@gmail.com>
2013-05-17 10:42:46 +0300

    Added tests to class Player

---
+++ MinesweeperProjectTest/PlayerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinesweeperProject;
+
+namespace MinesweeperProjectTest
+{
+    [TestClass]
+    public class PlayerTest
+    {
+        [TestMethod]
+        public void PlayerConstructorScoreTest()
+        {
+            Player player = new Player();
+            int expectedScore = 0;
+
+            Assert.AreEqual(expectedScore, player.Score);
+        }
+
+        [TestMethod]
+        public void PlayerConstructorMoveTest() 
+        {
+            Player player = new Player();
+            string expectedMove = "";
+
+            Assert.AreEqual(expectedMove, player.Move);
+        }
+    }
+}
